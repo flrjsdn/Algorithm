2d0be86a305d2622c91944e347ac014e7439378a
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-02-21 17:02:08 +0900

    [Bronze III] Title: 세 막대, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+        for(int i=0;i<3;i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        if(arr[2] >= arr[0]+arr[1]) {
+            arr[2] = arr[0]+arr[1] -1;
+            System.out.println(arr[0]+arr[1]+arr[2]);
+        }
+        else System.out.println(arr[0]+arr[1]+arr[2]);
+    }
+}
