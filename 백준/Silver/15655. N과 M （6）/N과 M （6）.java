1467cbe1fbf6afb098da7c1bbe873e14416ccfbd
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-04-07 15:32:42 +0900

    [Silver III] Title: N과 M (6), Time: 228 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static int n,m;
+    public static boolean[] visited;
+    public static int[] arr;
+    public static int[] result;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int index, int depth) {
+        if (depth == m) {
+            for(int i=0;i<m;i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=index;i<n;i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(i, depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        result = new int[m];
+        visited = new boolean[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        System.out.print(sb);
+
+    }
+}
