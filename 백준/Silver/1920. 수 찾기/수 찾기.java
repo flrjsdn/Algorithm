2b9b3e95bbd2e87771b1bfde2154eb5647ccd1a6
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-05-01 13:59:49 +0900

    [Silver IV] Title: 수 찾기, Time: 2200 ms, Memory: 191400 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,18 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	public static int binarySearch(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length-1;
+		while(start <= end) {
+			int mid = (start+end) / 2;
+			if(arr[mid] == target) return mid;
+			if(arr[mid] > target) end = mid-1;
+			else start = mid+1;
+		}
+		return -1;
+		
+	}
     public static void main(String[] args) throws IOException {
     	Scanner sc = new Scanner(System.in);
     	int n =sc.nextInt();
-    	HashSet<Integer> set = new HashSet<>();
+    	int[] arr = new int[n];
     	
     	for(int i=0;i<n;i++) {
-    		set.add(sc.nextInt());
+    		arr[i] = sc.nextInt();
     	}
     	
+    	Arrays.sort(arr);
     	int m =sc.nextInt();
     	for(int i=0;i<m;i++) {
-    		if(set.contains(sc.nextInt())) {
+    		if(binarySearch(arr, sc.nextInt()) >= 0) {
     			System.out.println(1);
     		}else {
     			System.out.println(0);
