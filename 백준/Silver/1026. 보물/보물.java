675fb38153049e2ebad13e62937593413ef19555
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-05-03 14:10:12 +0900

    [Silver IV] Title: 보물, Time: 220 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int[] a = new int[n];
+    	Integer[] b=  new Integer[n];
+    	
+    	for(int i=0;i<n;i++) {
+    		a[i] = sc.nextInt();
+    	}
+    	for(int i=0;i<n;i++) {
+    		b[i] = sc.nextInt();
+    	}
+    	
+    	Arrays.sort(a);
+    	Arrays.sort(b, Collections.reverseOrder());
+    	int result = 0;
+    	for(int i=0;i<n;i++) {
+    		result += a[i]*b[i];
+    	}
+    	System.out.println(result);
+    }
+}
