772d68253d077adf46650119696316628f9a72dc
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-08-18 16:24:56 +0900

    [Gold II] Title: 도토리 숨기기, Time: 200 ms, Memory: 18880 KB -BaekjoonHub

--- 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.java
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.java
@@ -15,27 +15,9 @@ class Box {
 
 
 public class Main {
+	public static ArrayList<Box> box;
 	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int d = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Box> box = new ArrayList<>();
-		for(int i=0;i<k;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			box.add(new Box(a,b,c));
-		}
-		
-		int lo = 1;
-		int hi = n;
+	public static int binarySearch(int lo, int hi,int target) {
 		while(lo <= hi) {
 			int mid = (lo+hi) / 2;
 			long cnt = 0;
@@ -47,9 +29,31 @@ public class Main {
 					cnt += (mid - box.get(i).start)== 0 ? 1 : (mid-box.get(i).start)/box.get(i).unit + 1; 
 				}
 			}
-			if(cnt >= d) hi = mid-1;
+			if(cnt >= target) hi = mid-1;
 			else lo = mid + 1;
 		}
-		System.out.println(lo);
+		return lo;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		
+		box = new ArrayList<>();
+		for(int i=0;i<k;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			box.add(new Box(a,b,c));
+		}
+		
+		int result = binarySearch(1,n,d);
+		
+		System.out.println(result);
     }
 }
