77b3228f667d885bd904dcc1b139a5a67281ff65
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2024-10-08 10:18:58 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1096 ms, Memory: 16680 KB -BaekjoonHub

---
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int read() throws Exception {
+		if(st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+		return Integer.parseInt(st.nextToken());
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		String[] sList = new String[n];
+		for(int i=0;i<n;i++) {
+			sList[i] = br.readLine();
+		}
+		
+		String s1 = "";
+		String s2 = "";
+		int cnt = 0;
+		
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				int min = Math.min(sList[i].length(), sList[j].length());
+				
+				int c = 0;
+				for(int k=0;k<min;k++) {
+					if(sList[i].charAt(k) == sList[j].charAt(k)) {
+						c++;
+					}else break;
+				}
+				
+				 if(c > cnt) {
+					 cnt = c;
+					 s1 = sList[i];
+					 s2 = sList[j];
+				 }
+			}
+		}
+		
+		System.out.println(s1 + "\n" + s2);
+	}
+}
