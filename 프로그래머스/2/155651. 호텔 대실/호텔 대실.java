0147313702ff8ae94271267a8411c6313fc3c48c
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2025-05-31 16:10:26 +0900

    [level 2] Title: 호텔 대실, Time: 4.29 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,28 +1,22 @@
-import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        int[][] bookList = new int[book_time.length][2];
+        int[] dp = new int[1500];
         for(int i=0;i<book_time.length;i++) {
             String[] start = book_time[i][0].split(":");
-            bookList[i][0] = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
+            int startTime = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
             String[] end = book_time[i][1].split(":");
-            bookList[i][1] = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]) + 10;
+            int endTime = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
+            dp[startTime] += 1;
+            dp[endTime + 10] -= 1;
         }
-        Arrays.sort(bookList, (a,b) -> a[0] - b[0]);
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        
-        for(int i=0;i<book_time.length;i++) {
-            while(!pq.isEmpty() && pq.peek()[1] <= bookList[i][0]) {
-                pq.poll();
-            }
-            pq.add(bookList[i]);
-            answer = Math.max(answer, pq.size());
-            
+
+
+        for(int i=1;i<1500;i++) {
+            dp[i] += dp[i-1];
+            if(dp[i] > answer) answer = dp[i];
         }
-    
-        
+
         return answer;
     }
 }
