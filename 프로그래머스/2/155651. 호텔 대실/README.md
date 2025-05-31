9f871f49c52a69278519fae93b78a7fb0360ff89
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2025-05-31 16:09:22 +0900

    [level 2] Title: 호텔 대실, Time: 9.90 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 106 MB, 시간: 29.18 ms
+메모리: 83.6 MB, 시간: 9.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 31일 15:55:09
+2025년 05월 31일 16:09:22
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,22 +1,27 @@
+import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        int[] dp = new int[1500];
+        int[][] bookList = new int[book_time.length][2];
         for(int i=0;i<book_time.length;i++) {
             String[] start = book_time[i][0].split(":");
-            int startTime = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
+            bookList[i][0] = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
             String[] end = book_time[i][1].split(":");
-            int endTime = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-            dp[startTime] += 1;
-            dp[endTime + 10] -= 1;
-            System.out.println(startTime + " " + endTime);
+            bookList[i][1] = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]) + 10;
         }
+        Arrays.sort(bookList, (a,b) -> a[0] - b[0]);
         
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
         
-        for(int i=1;i<1500;i++) {
-            dp[i] += dp[i-1];
-            if(dp[i] > answer) answer = dp[i];
+        for(int i=0;i<book_time.length;i++) {
+            while(!pq.isEmpty() && pq.peek()[1] <= bookList[i][0]) {
+                pq.poll();
+            }
+            pq.add(bookList[i]);
+            answer = Math.max(answer, pq.size());
+            
         }
+    
         
         return answer;
     }
