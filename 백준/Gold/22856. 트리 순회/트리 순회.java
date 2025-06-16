ee3105ebdfa8d39079433ae5315f31cb8ae0b2d7
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2025-06-16 15:07:09 +0900

    [Gold IV] Title: 트리 순회, Time: 300 ms, Memory: 48420 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.java
+++ 백준/Gold/22856. 트리 순회/트리 순회.java
@@ -1,80 +1,76 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+// The main method must be in a class named "Main".
+class Main {
+    static class Node {
+        public int left;
+        public int right;
 
-public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int read() throws Exception {
-		if(st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
-		return Integer.parseInt(st.nextToken());
-	}
+        public Node(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    static boolean[] visited;
+    static int answer, lastNode;
+    static Node[] graph;
+    static boolean flag;
 
-	static class Node{
-		int data;
-		int left;
-		int right;
-		
-		Node(int data, int left, int right) {
-			this.data = data;
-			this.left = left;
-			this.right = right;
-		}
-	}
-	static Node[] list;
-	static int ans, lastNode, nodeCnt;
-	static boolean[] visited;
-	static int n;
-	static boolean flag;
-	
-	static void inorder(int node) {
-		if(list[node].left != -1) {
-			inorder(list[node].left);
-		}
-		
-		lastNode = node;
-		
-		if(list[node].right != -1) {
-			inorder(list[node].right);
-		}
-	}
-	
-	static void dfs(int node) {
-		visited[node] = true;
-		
-		if(list[node].left != -1 && !visited[list[node].left]) {
-			dfs(list[node].left);
-			ans++;
-		}
-		
-		if(list[node].right != -1 && !visited[list[node].right]) {
-			dfs(list[node].right);
-			ans++;
-		}
-		
-		if(node == lastNode) {
-			flag = true;
-			return;
-		}
-		if(flag) return;
-		ans++;
-	}
-	public static void main(String[] args) throws Exception {
-		n = read();
-		list = new Node[n+1];
-		visited = new boolean[n+1];
-		
-		for(int i=0;i<n;i++) {
-			int parent = read();
-			int left = read();
-			int right = read();
-			list[parent] = new Node(parent, left, right);
-		}
-		ans = 0;
-		inorder(1);
-		
-		nodeCnt = 0;
-		dfs(1);
-		System.out.println(ans);
+    static void init(int node) {
+        if(graph[node].left != -1) {
+            init(graph[node].left);
+        }
+
+        lastNode = node;
+
+        if(graph[node].right != -1) {
+            init(graph[node].right);
+        }
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        int left = graph[node].left;
+        int right = graph[node].right;
+        if(left != -1 && !visited[left]) {
+            dfs(left);
+            answer++;
+        }
+
+        if(right != -1 && !visited[right]) {
+            dfs(right);
+            answer++;
+        }
+
+        if(node == lastNode) {
+            flag = true;
+            return;
+        }
+        if(flag) return;
+        answer++;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        visited = new boolean[n+1];
+        graph = new Node[n+1];
+        
+        StringTokenizer st;
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            graph[p] = new Node(l,r);
+        }
+
+        answer = 0;
+        init(1);
+        dfs(1);
+        System.out.println(answer);
     }
 }
