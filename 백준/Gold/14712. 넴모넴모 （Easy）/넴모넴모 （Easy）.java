0c66119d6eb01cd8099131a03e0a735e30a2d867
flrjsdn <50162247+flrjsdn@users.noreply.github.com>
2025-06-17 17:14:57 +0900

    [Gold V] Title: 넴모넴모 (Easy), Time: 268 ms, Memory: 12548 KB -BaekjoonHub

--- 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.java
+++ 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.java
@@ -5,38 +5,34 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int n,m, ans;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
     static boolean[][] visited;
 
 
-    static void dfs(int start, int depth) {
-        ans += check(depth) ? 1 : 0;
-        if(depth == n*m) {
+    static void dfs(int x,int y) {
+        if(check(x,y)) return;
+
+        if(x==n-1 && y==m-1) {
+            ans++;
             return;
         }
-
-        for(int i=start;i<n*m;i++) {
-            int x = i/m;
-            int y = i%m;
-            if(visited[x][y]) continue;
-            visited[x][y] = true;
-            dfs(i+1, depth+1);
-            visited[x][y] = false;
+        int nx = x;
+        int ny = y;
+        if(ny==m-1) {
+            nx +=1;
+            ny=0;
+        } else{
+            ny+=1;
         }
+
+        visited[nx][ny] = true;
+        dfs(nx,ny);
+
+        visited[nx][ny] = false;
+        dfs(nx,ny);
     }
 
-    static boolean check(int depth) {
-        if(depth < 4) return true;
-
-        for(int i=0;i<n-1;i++) {
-            for(int j=0;j<m-1;j++) {
-                if(visited[i][j] && visited[i][j+1] && visited[i+1][j] && visited[i+1][j+1]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+    static boolean check(int x,int y) {
+        return (x>=1 && y>=1 && visited[x][y] && visited[x-1][y] && visited[x][y-1] && visited[x-1][y-1]);
     }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,7 +42,11 @@ class Main {
         visited = new boolean[n][m];
 
         ans = 0;
-        dfs(0, 0);
+        visited[0][0] = true;
+        dfs(0,0);
+
+        visited[0][0] = false;
+        dfs(0,0);
         System.out.println(ans);
     }
 }
